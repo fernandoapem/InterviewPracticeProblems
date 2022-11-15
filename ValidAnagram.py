6b45fb2f31a6e16057bc639ac9ba96896d9d8c0a
Fernando Perez <61566161+fernandoapem@users.noreply.github.com>
2022-11-14 22:25:48 -0500

    Completed 242

--- ValidAnagram.py
+++ ValidAnagram.py
@@ -1,6 +1,41 @@
 class Solution:
+    #First Solution
     def isAnagram(self, s: str, t: str) -> bool:
-        check = False
-        
-
+        check = True
+        letters = {}
+        if len(s) != len(t):
+            return False
+        for char in s:
+                letters[char] = 0
+        for char in s:
+                x = letters[char] + 1
+                letters[char] = x
+        for char in t:
+                if char not in s:
+                    return False
+                if t.count(char) != letters[char]:
+                    check = False
         return check
+    #Too Optimal
+    def isAnagram2(self, s: str, t: str) -> bool:
+        return sorted(t) == sorted(s)
+
+    def isAnagram3(self, s: str, t: str) -> bool:
+        letters = {}
+        for char in s:
+            if char not in letters:
+                letters[char] = 1
+            else:
+                letters[char] += 1
+        for char in t:
+            if char not in letters:
+                return False
+            else:
+                letters[char] -= 1
+        for char in letters:
+            if letters[char] != 0:
+                return False
+        return True
+
+p = Solution()
+print(p.isAnagram3("anagram","nagaram"))
