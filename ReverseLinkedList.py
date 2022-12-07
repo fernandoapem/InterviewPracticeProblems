9ec805e6cbf31076e12db104280ec78d454af5ee
Fernando Perez <61566161+fernandoapem@users.noreply.github.com>
2022-12-07 00:40:07 -0500

    Update ReverseLinkedList.py

--- ReverseLinkedList.py
+++ ReverseLinkedList.py
@@ -5,14 +5,27 @@ class ListNode:
         self.next = next
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if head is None:
+            return None
         nodes = []
         curr = head
         while curr:
-            nodes.push(curr)
+            nodes.append(curr)
             curr = curr.next
         nhead = ListNode()
         curr = nhead
-        for node in nodes:
-            curr.next = nodes.pop()
+        for item in range(0,len(nodes)):
+            node = nodes.pop()
+            node.next = None
+            curr.next = node
             curr = curr.next
-        return nhead
+        return nhead.next
+    def reverse2(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev = None
+        curr = head
+        while curr:
+            next = curr.next
+            curr.next = prev
+            prev = curr
+            curr = next
+        return prev
